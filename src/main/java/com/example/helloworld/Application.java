7e2471c3f2d78d41a7c6ea7fc11b00acdb031ccd
PavelSharko <pushkin.94.72@gmail.com>
2024-09-21 12:38:49 +0700

    Second

--- src/main/java/com/example/helloworld/Application.java
+++ src/main/java/com/example/helloworld/Application.java
@@ -1,12 +1,14 @@
 package com.example.helloworld;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.DispatcherServlet;
 
-@SpringBootApplication
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        // Здесь может быть код для запуска приложения в виде standalone, если это необходимо.
     }
 }
--- src/main/java/com/example/helloworld/controller/HelloController.java
+++ src/main/java/com/example/helloworld/controller/HelloController.java
@@ -4,11 +4,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@RequestMapping("/api")
 public class HelloController {
 
     @GetMapping("/")
     public String sayHello() {
-        return "Hello Worldаыавваы";
+        return "Hello World";
     }
 }
